3a35574a6ed2cf6b0a38b2277b98f1f717f082d9
Shikha Suneja <shsu2565@colorado.edu>
2019-02-05 21:59:15 -0700

    Added Docstrings and some error handling

--- network_visualization.py
+++ network_visualization.py
@@ -1,8 +1,8 @@
 """
-Team VoIS-Network
-Date: 01/28/2019
-Purpose: Network Topology Visualization
-Author: Shikha Suneja
+    Team VoIS-Network
+    Date: 01/28/2019
+    Purpose: Network Topology Visualization
+    Author: Shikha Suneja
 """
 
 import sqlite3
@@ -15,23 +15,58 @@ LINKS_TABLE = 'topo_connections'
 TOPOLOGY_IMAGE = 'topology.png'
 
 def get_all_records(conn, table):
+    """ Gets all records of a table from a database
+        args:
+            conn: Sqlite3 database connection object
+            table: Table name
+        returns: all records of the table from the database
+    """
+
     return conn.execute("SELECT * from {}".format(table))
 
 def get_topo_details():
-    conn = sqlite3.connect(TOPOLOGY_DB)
+    """ Gets the information about switches and links in the topology
+        returns: list of switches and links
+    """
+
+    try:
+        conn = sqlite3.connect(TOPOLOGY_DB)
+    except:
+        print("Error connecting to the database {}".format(TOPOLOGY_DB))
+        return
     switches = ["s" + str(row[0]) for row in get_all_records(conn, SW_TABLE)]
     links = [("s" + str(row[0]), "s" + str(row[1]), {'src_port':row[2],
             'dst_port': row[3]}) for row in get_all_records(conn, LINKS_TABLE)]
     return switches, links
 
-def get_edge_labels(G, label):
-     return dict([((u,v,),d[label]) for u,v,d in G.edges(data=True)])
+def get_edge_labels(G, label_name):
+    """ Gets labels to be set on the edges based on the label_name
+        args:
+            G: The networkx graph object
+            label: The label name that needs to be extracted from the graph object
+    """
+
+     return dict([((u,v,),d[label_name]) for u,v,d in G.edges(data=True)])
 
 def set_edge_labels(G, pos, edge_labels, label_pos):
+    """ Set labels on the edges of the graph
+        args:
+            G: The networkx graph object
+            pos: Layout of the graph
+            edge_labels: Labels to be set on the edges
+            label_pos: Position of the labels on the edges
+    """
+
     nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels,
     label_pos = label_pos)
 
 def draw_topology(switches, links):
+    """ Function to draw the network topology
+        args:
+            switches: list of switches
+            links: list of links
+    """
+
     G = nx.MultiGraph()
     G.add_nodes_from(switches)
     G.add_edges_from(links)
@@ -43,9 +78,9 @@ def draw_topology(switches, links):
     set_edge_labels(G, pos, src_ports, 0.2)
     dst_ports = get_edge_labels(G, 'dst_port')
     set_edge_labels(G, pos, dst_ports, 0.8)
-    #plt.show()
     plt.savefig(TOPOLOGY_IMAGE)
 
 if __name__ == "__main__":
     switches, links = get_topo_details()
-    draw_topology(switches, links)
+    if switches or links:
+        draw_topology(switches, links)
